bb9babdbd381717cfb7c6e8caf600a2c2aa7a90c
markbayley <markyb152@hotmail.com>
2021-02-26 16:06:53 +1000

    build

---
+++ build/precache-manifest.2df22bd7457028b054bddda9ea7fa765.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5b9fbe793148a961a62ab6b18c5ca4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0a2cc04ec50cc6c61892",
+    "url": "./static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "ab6b2bbfec726ef69cf0",
+    "url": "./static/css/main.382ce2f1.chunk.css"
+  },
+  {
+    "revision": "0a2cc04ec50cc6c61892",
+    "url": "./static/js/2.5e0800cd.chunk.js"
+  },
+  {
+    "revision": "3076bc301cd9ac8a6e5baf44c2b789b6",
+    "url": "./static/js/2.5e0800cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab6b2bbfec726ef69cf0",
+    "url": "./static/js/main.707c9e76.chunk.js"
+  },
+  {
+    "revision": "cd65410e499caeea6ae1",
+    "url": "./static/js/runtime-main.fe88992c.js"
+  },
+  {
+    "revision": "cd488de327fe726ee152d0f43dce93ff",
+    "url": "./static/media/eth.cd488de3.png"
+  },
+  {
+    "revision": "f8f729e2daf5195362da5fc46e4b487a",
+    "url": "./static/media/inblocklogo.f8f729e2.png"
+  },
+  {
+    "revision": "e9291eaddacd460280a34a151dcc5cc4",
+    "url": "./static/media/loading.e9291ead.gif"
+  }
+]);
